476ee570b2e2397c72ba0c8ce35e4187704f03a3
antolow <98319496+antolow@users.noreply.github.com>
2023-02-17 19:39:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let elements = myForm.elements;
+let titleName = document.querySelector('#titleName');
+let btn = document.querySelector('#button1');
+let wrapper = document.querySelector(".wrapper")
+let wrap_text =document.querySelector('.wrap_text')
+
+button1.myBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    wrap_text.style.display = 'block'
+    
+})
